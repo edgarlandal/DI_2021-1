5f5e6079993aef366f83917cdb4911bb915fbccf
edgarlandal <72184902+edgarlandal@users.noreply.github.com>
2021-02-24 09:42:44 -0800

    Practica2

--- Practica1Mapa/Assets/NewBehaviourScript.cs
+++ Practica1Mapa/Assets/NewBehaviourScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
-    public string[] alumnos = {"Edgar","Erick"};
+    public string[] alumnos = {"Edgar","Erick","Mario","Paul","Valeria","Paola","Areli","Victor","Andres"};
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        string respuesta = "Edgar";
+
+        Debug.Log(estaAlumno(alumnos,respuesta));
     }
 
+    public bool estaAlumno(string [] alumnos, string name){
+
+        for (int i = 0; i < alumnos.Length; i++)
+        {
+            if (alumnos[i] == name)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
     // Update is called once per frame
     void Update()
     {
-     
+        
+    }
+}
