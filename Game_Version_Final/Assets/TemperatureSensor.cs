87e8c9977918a1025782d99c88a6d27d9c33ccc7
edgarlandal <72184902+edgarlandal@users.noreply.github.com>
2021-04-23 20:11:54 -0700

    Practica10

---
+++ Game_Version_Final/Assets/TemperatureSensor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt.Utility;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+using UnityEngine.UI;
+using System;
+public class TemperatureSensor : MonoBehaviour
+{
+    public string brokerIpAddress = "192.168.0.113";
+	public int brokerPort = 5001;
+	public string temperatureTopic = "casa/sala/temperatura";
+    private MqttClient client;
+    public float reportRate = 5;
+    public float temperatureValue = 23.3f;
+    private float reportTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // create client instance 
+		client = new MqttClient(IPAddress.Parse(brokerIpAddress), brokerPort, false, null); 		
+		string clientId = Guid.NewGuid().ToString(); 
+		client.Connect(clientId); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((reportTimer += Time.deltaTime) >= reportRate)
+        {			
+            Debug.Log("sending...");
+			client.Publish("casa/sala/luz", System.Text.Encoding.UTF8.GetBytes("lightOn"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+			Debug.Log("sent");
+            reportTimer = 0f;
+        }
+    }
+}
---
+++ Speach-to-text/Assets/Scripts/Interactable.cs
@@ -0,0 +1,134 @@
+﻿  
+using System.Collections;
+using System.Collections.Generic;
+using IBM.Watsson.Examples;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInsideZone = false;
+    public bool gazedAt = false;
+
+    public Animator animator;
+    public float gazeInteractTime = 2f;
+    public float gazeTimer = 0;
+    // public KeyCode interactionKey = KeyCode.P;
+    public string interactionButton = "Interact";
+
+    public string voiceCommand = "ataca";
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        VoiceCommandProcessor commandProcessor = GameObject.FindObjectOfType<VoiceCommandProcessor>();
+        commandProcessor.onVoiceCommandRecognized += OnVoiceCommandRecognized;
+    }
+    
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    public virtual void Update()
+    {
+        // if (isInsideZone && Input.GetKeyDown(interactionKey))
+        if (isInsideZone && CrossPlatformInputManager.GetButtonDown(interactionButton))
+        {
+            Interact();
+        }
+        if (gazedAt)
+        {
+            if ((gazeTimer += Time.deltaTime) >= gazeInteractTime)
+            {
+                Interact();
+                gazedAt = false;
+                gazeTimer = 0f;
+            }
+        }
+    }
+
+    public void OnVoiceCommandRecognized(string command)
+    {
+        if (command.ToLower() == voiceCommand.ToLower())
+        {
+            Interact();
+            Debug.Log("Hola");
+            animator.SetBool("Scratch",true);
+
+        }
+    }
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        Debug.Log("Entró en el área");
+        isInsideZone = true;
+    }
+
+    // /// <summary>
+    // /// OnMouseDown is called when the user has pressed the mouse button while
+    // /// over the GUIElement or Collider.
+    // /// </summary>
+    // void OnMouseDown()
+    // {
+    //     Interact();
+    // }
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        this.gazedAt = gazedAt;
+        if (!gazedAt)
+        {
+            gazeTimer = 0f;
+        }
+    }
+
+    public void OnPointerEnter()
+    {
+        gazedAt = true;
+        gazeTimer = 0f;
+    }
+
+    public void OnPointerExit()
+    {
+        gazedAt = false;
+        gazeTimer = 0f;
+    }
+
+    /// <summary>
+    /// This method is called by the Main Camera when it is gazing at this GameObject and the screen
+    /// is touched.
+    /// </summary>
+    public void OnPointerClick()
+    {
+        Interact();
+    }
+
+    /// <summary>
+    /// OnTriggerExit is called when the Collider other has stopped touching the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        Debug.Log("Salió en el área");
+        isInsideZone = false;
+    }
+
+    public virtual void Interact()
+    {
+
+    }    
+}
